6f1bcdb1cada38c57b8d9124c74bbfcba5be3886
Hesen_ch75@itstep.edu.az <Hesen_ch75@itstep.edu.az>
2023-08-27 19:46:27 +0400

    Add project files.

---
+++ OnlineUsersServer/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineUsersServer.Models
+{
+    public class Message
+    {
+        public TcpClient Client { get; set; }
+        public string Owner { get; set; }
+        public string Content { get; set; }
+
+    }
+}
---
+++ OnlineUsersServer/ViewModels/AppViewModel.cs
@@ -0,0 +1,132 @@
+﻿using OnlineUsersServer.Commands;
+using OnlineUsersServer.Helpers;
+using OnlineUsersServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace OnlineUsersServer.ViewModels
+{
+    public class AppViewModel : BaseViewModel
+    {
+        static TcpListener listener = null;
+        static BinaryWriter bw = null;
+        static BinaryReader br = null;
+        public MainWindow _mainvindow { get; set; }
+        public ListView UsersListView { get; set; }
+
+        private ObservableCollection<Message> messages;
+
+        public ObservableCollection<Message> Messages
+        {
+            get { return messages; }
+            set { messages = value; OnPropertyChanged(); }
+        }
+
+
+
+        private ObservableCollection<string> users;
+
+        public ObservableCollection<string> Users
+        {
+            get { return users; }
+            set { users = value; OnPropertyChanged(); }
+        }
+
+
+        public RelayCommand ConnectCommand { get; set; }
+        public static List<TcpClient> Clients { get; set; }
+        public AppViewModel(MainWindow vindow)
+        {
+            Clients = new List<TcpClient>();
+            Users = new ObservableCollection<string>();
+            Messages = new ObservableCollection<Message>();
+            _mainvindow = vindow;
+            LoadCommands();
+
+
+            
+            Task.Run(() =>
+            {
+                while (true)
+                {
+
+                    foreach (var item in Clients)
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                NetworkStream stream = item.GetStream();
+                                byte[] data = new byte[item.ReceiveBufferSize];
+                                int bytesRead = stream.Read(data, 0, data.Length);
+                                string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+                                Message msg = JsonHelper.GetMessageFromString(message);
+                                MessageBox.Show($"{msg.Owner} : {msg.Client}");
+                                Messages.Add(msg);
+                                Users.Add(msg.Owner);
+                                break;
+                            }
+                            catch (Exception)
+                            {
+
+                            }
+                        Task.Delay(2000);
+                        }
+                    }
+                }
+            });
+
+
+        }
+
+
+        public void LoadCommands()
+        {
+            ConnectCommand = new RelayCommand((o) =>
+            {
+                var ip = IPAddress.Parse(IPHelper.GetLocalIpAddress());
+                var port = 80;
+
+                var ep = new IPEndPoint(ip, port);
+                listener = new TcpListener(ep);
+                listener.Start();
+                MessageBox.Show("Connected");
+
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            if (listener.Pending())
+                            {
+                                var client = listener.AcceptTcpClient();
+                                Clients.Add(client);
+                                MessageBox.Show("Client Added");
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+                });
+
+
+            });
+        }
+
+
+
+    }
+}
